c396d313c3dac544e4a87273db2ab545de6e014b
klukv <tema200260@gmail.com>
2026-04-24 17:35:53 +0300

    add logic of toggle popup when clicking on button

--- module-1.3/my-select.js
+++ module-1.3/my-select.js
@@ -1,71 +1,102 @@
 class MySelect extends HTMLElement {
-  #selectButton;
-  #selectPopup;
-  #selectPopupSearch;
   #optionsBox;
   #templateOption;
+  #convertedOptions;
+  #shadow;
+  #selectPopup;
+  #selectButton;
 
   constructor() {
     super();
   }
 
   connectedCallback() {
-    this.#createTemplateOption();
-    this.#createHTMLSelect();
+    // this.#shadow = this.attachShadow({ mode: "open" });
+    this.#defineInitialOptions()
+      .#createTemplateOption()
+      .#renderSelect()
+      .#removeInitialOptions();
   }
 
-  #createHTMLSelect() {
-    const wrapper = document.createElement("div");
+  #createTemplateOption() {
+    const template = document.createElement("template");
 
-    wrapper.innerHTML = `
-    <button class="select-button"></button>
+    template.className = "option-template";
+    template.innerHTML = `<label class="option"><input type="checkbox"/></label>`;
+
+    this.append(template);
+
+    this.#templateOption = this.querySelector(".option-template");
+
+    return this;
+  }
+
+  #defineInitialOptions() {
+    const options = this.querySelectorAll("option");
+
+    this.#convertedOptions = Array.from(options).reduce((acc, option) => {
+      acc.push({ [option.value]: option.textContent.trim() });
+      return acc;
+    }, []);
+
+    return this;
+  }
+
+  #renderSelect() {
+    this.innerHTML = `
+     <style>
+        my-select {
+          position: relative;
+          display: inline-block;
+        }
+        .select-popup {
+          display: none;
+          position: absolute;
+          top: 100%;
+          left: 0;
+        }
+ 
+        .select-popup.open {
+          display: block;
+        }
+    </style>
+    <button class="select-button">Кнопка</button>
     <div class="select-popup">
       <input id="search" placeholder="Search..." />
       <div class="select-popup-options"></div>
     </div>
     `;
 
-    this.append(wrapper);
-
-    const options = this.querySelectorAll("option");
-
-    const convertedOptions = Array.from(options).reduce((acc, option) => {
-      acc.push({ [option.value]: option.textContent.trim() });
-      return acc;
-    }, []);
-
-    this.#selectButton = this.querySelector(".select-button");
-    this.#selectPopup = this.querySelector(".select-popup");
-    this.#selectPopupSearch = this.querySelector(".select-popup-search");
     this.#optionsBox = this.querySelector(".select-popup-options");
+    this.#selectPopup = this.querySelector(".select-popup");
+    this.#selectButton = this.querySelector(".select-button");
+    
+    this.#selectButton.addEventListener("click", this.#openPopup.bind(this));
 
-    this.#renderOptions(convertedOptions);
+    this.#renderOptions(this.#convertedOptions);
 
-    options.forEach(option => option.remove())
-  }
-
-  #createTemplateOption() {
-    const template = document.createElement("template");
-
-    template.id = "option-template"
-    template.innerHTML = `<label class="option"><input type="checkbox"/></label>`;
-
-    document.body.append(template);
-
-    this.#templateOption = document.getElementById("option-template");
+    return this;
   }
 
   #renderOptions(options) {
     options.forEach((option) => {
       const optionTemplate = this.#templateOption.content.cloneNode(true);
-      const labelWrapper = optionTemplate.querySelector('.option');
+      const labelWrapper = optionTemplate.querySelector(".option");
 
       const optionEntries = Object.entries(option);
       labelWrapper.dataset.value = optionEntries[0][0];
       labelWrapper.append(optionEntries[0][1]);
 
-      this.#optionsBox.append(labelWrapper)
+      this.#optionsBox.append(labelWrapper);
     });
+  }
+
+  #removeInitialOptions() {
+    this.querySelectorAll("option").forEach((option) => option.remove());
+  }
+
+  #openPopup() {
+    this.#selectPopup.classList.toggle("open");
   }
 }
 
